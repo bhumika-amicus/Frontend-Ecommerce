a80caf9183b6772618c492aa2e8edd040bb0929b
Bhumika Deshmukh <bhumika_deshmukh@amicusglobal.com>
2026-09-22 20:51:52 +0530

    fix: removed unwanted assignment 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Assignment3 from './pages/Assignment3'
-import Assignment4 from './pages/Assignment4'
 import Assignment5 from './pages/Assignment5'
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/assignment3" element={<Assignment3 />} />
-        <Route path="/assignment4" element={<Assignment4 />} />
         <Route path="/assignment5" element={<Assignment5 />} />
       </Routes>
     </BrowserRouter>
